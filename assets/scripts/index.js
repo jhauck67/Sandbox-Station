cab5aca30f6ae61698c8af4ff7bae75dbb7ed1ea
jhauck67 <jenniferhauck2017@gmail.com>
2025-09-20 06:21:27 +0200

    Création d'une animation "light" sur le header

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,3 +10,23 @@ burgerIcon.addEventListener('click', () => {
 });
 
 
+// TITRE : [LIGHT] : Animation "light" du header //
+const lights = document.querySelectorAll('.light');
+const speed = 5000;
+
+// La fonction calcule des coordonnées aléatoires et applique une transition à chaque .light
+const moveLight = () => {
+    lights.forEach(light => {
+        light.style.setProperty('--top', Math.random() * 100 + "%");
+        light.style.setProperty('--left', Math.random() * 100 + "%");
+        light.style.transition = `all ${speed}ms ease-in-out`;
+    });
+};
+
+// On appelle une première fois la fonction
+moveLight()
+
+// La fonction se rejoue avec un intervalle de ${speed}
+setInterval(moveLight, speed);
+
+

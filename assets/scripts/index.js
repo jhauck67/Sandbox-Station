2de4361a6129782c80785a07d77cc2731cac6c5d
jhauck67 <jenniferhauck2017@gmail.com>
2025-09-20 04:54:05 +0200

    Update : intégration du burger menu

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,2 +1,12 @@
-console.log("OK ! On es connecté !");
+// TITRE : [NAVBAR] : Animation de la barre de navigation //
+
+// ###### BURGER MENU ###### //
+const burgerIcon = document.getElementById('icon-burger');
+const navbar = document.querySelector('.navbar');
+
+// Au click, on applique ou pas la classe .open
+burgerIcon.addEventListener('click', () => {
+    navbar.classList.toggle('open');
+});
+
 
